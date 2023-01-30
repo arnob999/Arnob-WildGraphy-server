1a9cf431195705df7436cbebc80309169a76a602
Buggy-ARNOB <81729278+Buggy-ARNOB@users.noreply.github.com>
2023-01-30 10:34:56 +0600

    server-side-first-commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send("arnob wild graphy is running successfully");
+
+})
+
+app.listen(port, () => {
+    console.log(`arnob wild graphy ${port}`);
+})
