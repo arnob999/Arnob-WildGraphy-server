285325316483f4609517fbc0e229703e99b60a0c
Buggy-ARNOB <81729278+Buggy-ARNOB@users.noreply.github.com>
2023-02-08 23:58:48 +0600

    env & jwt

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-
+const jwt = require('jsonwebtoken')
 const app = express()
 
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+
+
 
 app.use(cors());
 app.use(express.json());
